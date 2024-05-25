515aae69271532c53c582c7f2a321ecbaab7d99c
Brian Fontillas <brianfontillas@Brians-MacBook-Pro.local>
2024-05-25 00:04:53 -1000

    add clear functionality

---
+++ public/constants.js
@@ -0,0 +1,12 @@
+const STATUS = {
+  todo: "To-do",
+  inProgress: "In progress",
+  review: "In review",
+  testing: "In testing",
+  monitoring: "Monitoring",
+  done: "Done"
+};
+
+module.exports = {
+  STATUS
+};
